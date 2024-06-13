4c985c46c36690a4308405103f7d5a5c829b33c0
jagadees8300 <jagadeeswaran0818@gmail.com>
2024-06-13 18:37:06 +0530

    greater of value

--- add.py
+++ add.py
@@ -1,4 +1,11 @@
-a=22
-b=22
-c=a+b
-print(c)
+a=100
+b=80
+c=50
+if a>b &a>c:
+  greater= a
+elif b>a&b>c:
+    greater=b
+else:
+    greater=c
+    print("The greatest of", a, b, "and", c, "is", greatest) 
+         
